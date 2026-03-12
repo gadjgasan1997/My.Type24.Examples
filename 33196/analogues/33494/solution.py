f3ddb308f0b01e489305768b533dca96db9c6128
Гасан <gadj.gasan1997@gmail.com>
2026-03-12 22:00:42 +0300

    added example 33196 with analogues

---
+++ 33196/analogues/33494/solution.py
@@ -0,0 +1,20 @@
+# Читаем одну строку из файла
+s = open('data.txt').readline()
+
+# Переменные для хранения рекорда
+max_occurrences = 0
+best_following_char = ''
+
+# Перебираем все заглавные латинские буквы
+for current_char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+    # Считаем, сколько раз в тексте встречается комбинация 'E' + текущая буква
+    pair_to_find = 'E' + current_char
+    current_pair_count = s.count(pair_to_find)
+
+    # Если нашли новую самую частую пару
+    if current_pair_count > max_occurrences:
+        max_occurrences = current_pair_count
+        best_following_char = current_char
+
+# Выводим букву, которая чаще всего была "соседом" буквы А справа
+print(best_following_char)
