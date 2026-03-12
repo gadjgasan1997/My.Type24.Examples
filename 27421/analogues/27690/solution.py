8a0a56bf58f3ad78de8520cc61602c91202cc895
Гасан <gadj.gasan1997@gmail.com>
2026-03-12 21:12:19 +0300

    added analogues for example 27421

--- 27421/analogues/27690/solution.py
+++ 27421/analogues/27690/solution.py
@@ -1,11 +1,20 @@
 ﻿f = open('data.txt').readline()
-k = 1
-m = 0
+
+current_streak = 1  # Текущая длина цепочки из разных символов
+max_streak = 0      # Рекордная длина
+
+# Перебираем строку со второго символа, чтобы сравнивать с предыдущим
 for i in range(1, len(f)):
+    # Если текущий символ НЕ такой же, как предыдущий
     if f[i] != f[i-1]:
-        k += 1
+        current_streak += 1
     else:
-        m = max(m, k)
-        k = 1
-m = max(m, k)
-print(m)
+        # Если встретили дубль (например, 'AA'), фиксируем рекорд
+        max_streak = max(max_streak, current_streak)
+        # Сбрасываем счетчик (текущий символ уже образует новую цепочку длиной 1)
+        current_streak = 1
+
+# Финальная проверка рекорда (на случай, если самая длинная цепочка была в конце)
+max_streak = max(max_streak, current_streak)
+
+print(max_streak)
