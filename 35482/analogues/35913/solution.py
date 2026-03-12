6e63a05b48e8a4a9b3db810f00f06041ba28eff7
Гасан <gadj.gasan1997@gmail.com>
2026-03-12 22:34:30 +0300

    added example 35482 with analogues

---
+++ 35482/analogues/35913/solution.py
@@ -0,0 +1,22 @@
+lines = open('data.txt').readlines()
+g_counts = []
+
+# Считаем количество 'N' в каждой строке
+for line in lines:
+    count_g = line.count('N')
+    g_counts.append(count_g)
+
+# Находим строку, где 'N' меньше всего
+# (используем индекс минимального значения из нашего списка счетчиков)
+target_line = lines[g_counts.index(min(g_counts))]
+
+char_frequency = {}
+
+# Считаем, сколько раз встречается каждый символ в выбранной строке
+for char in target_line:
+    if char not in char_frequency:
+        char_frequency[char] = 0
+    char_frequency[char] += 1
+
+# Выводим символ, который встречается чаще всего
+print(max(char_frequency, key=char_frequency.get))
